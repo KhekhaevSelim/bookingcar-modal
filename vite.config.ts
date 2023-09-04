adbb3c1422856133eab43a87128871232dfa6283
SelimKhekhaev <selim16695@mail.ru>
2023-09-04 19:03:15 +0300

    deploy changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base : 'https://KhekhaevSelim.github.io/bookingcar-react-modal',
   plugins: [react()],
 })
